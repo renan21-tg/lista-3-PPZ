e52fc6a88887cb8f6b7fc7cdb6cfb09b0c4b423d
Renan Tomasi <160744095+renan21-tg@users.noreply.github.com>
2024-03-06 09:08:37 -0300

    Create Ex-2.py

---
+++ Ex-2.py
@@ -0,0 +1,7 @@
+x = input('Insira seu usuário: ')
+y = input('Insira sua senha: ')
+
+while x == y:
+    print('Senha inválida, insira uma senha diferente do usuário!')
+    x = input('Insira seu usuário: ')
+    y = input('Insira sua senha: ')
